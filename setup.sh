7e1e31e825680c7d4c11a4ebe961284f592c9843
sYc <songyc9@126.com>
2024-12-17 23:19:22 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-:'
-Prerequisite: Already built the ITI environement.
 
-Set up the new files and folders on the ITI code base.
+# Prerequisite: Already built the ITI environement.
 
-'
-cd ..
-cd honest_llama
-mkdir features
-mkdir trained_probes
-cp "Introspective-Reasoning/get_activations/get_activations.py" "honest_llama/get_activations/get_activations.py"
-mv "Introspective-Reasoning/get_activations/prepare_math_shepherd.py" "honest_llama/get_activations/"
-mkdir probe_trainng
-mv "Introspective-Reasoning/probe_trainng/probe_training.py" "honest_llama/probe_training/"
-cp "Introspective-Reasoning/utils.py" "honest_llama/utils.py"
-mv "Introspective-Reasoning/on-the-fly_probe_training.sh" "honest_llama/"
-mv "Introspective-Reasoning/probes_comparable_training.sh" "honest_llama/"
-mkdir trained_probes
-mv "Introspective-Reasoning/trained_probes/layer3_lstm_llama3.1_8b_instruct_16_math_shepherd_2k_best84.json" "honest_llama/trained_probes/"
+# Set up the new files and folders on the ITI code base.
+
+
+mkdir ./honest_llama/features
+mkdir ./honest_llama/trained_probes
+cp "./Introspective_Reasoning/get_activations/get_activations.py" "./honest_llama/get_activations/get_activations.py"
+mv "./Introspective_Reasoning/get_activations/prepare_math_shepherd.py" "./honest_llama/get_activations/"
+mkdir ./honest_llama/probe_training
+mv "./Introspective_Reasoning/probe_training/probe_training.py" "./honest_llama/probe_training/"
+cp "./Introspective_Reasoning/utils.py" "./honest_llama/utils.py"
+mv "./Introspective_Reasoning/on-the-fly_probe_training.sh" "./honest_llama/"
+mv "./Introspective_Reasoning/probes_comparable_training.sh" "./honest_llama/"
+mv "./Introspective_Reasoning/trained_probes/layer3_lstm_llama3.1_8b_instruct_16_math_shepherd_2k_best84.json" "./honest_llama/trained_probes/"
